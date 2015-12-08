0e33a4578da082062675e2387886b84abebee291
Muhammad Waqas Javed <waqasjaved.qc@gmail.com>
2015-12-09 01:29:43 +0500

    added few more changes

--- client/parties/controllers/partiesDetails.js
+++ client/parties/controllers/partiesDetails.js
@@ -17,4 +17,57 @@ angular.module('socialize').controller("PartyDetailsCtrl", function ($scope, $st
     $scope.reset = function () {
         $scope.party.reset();
     };
+
+    $scope.canInvite = function () {
+        if (!$scope.party)
+            return false;
+
+        return !$scope.party.public &&
+            $scope.party.owner === Meteor.userId();
+    };
+
+    $scope.invite = function (user) {
+        $meteor.call('invite', $scope.party._id, user._id).then(
+            function (data) {
+                console.log('success inviting', data);
+            },
+            function (err) {
+                console.log('failed', err);
+            }
+        );
+    };
+
+    $scope.map = {
+        center: {
+            latitude: 45,
+            longitude: -73
+        },
+        zoom: 8,
+        events: {
+            click: function (mapModel, eventName, originalEventArgs) {
+                if (!$scope.party)
+                    return;
+
+                if (!$scope.party.location)
+                    $scope.party.location = {};
+
+                $scope.party.location.latitude = originalEventArgs[0].latLng.lat();
+                $scope.party.location.longitude = originalEventArgs[0].latLng.lng();
+                //scope apply required because this event handler is outside of the angular domain
+                $scope.$apply();
+            }
+        },
+        marker: {
+            options: {draggable: true},
+            events: {
+                dragend: function (marker, eventName, args) {
+                    if (!$scope.party.location)
+                        $scope.party.location = {};
+
+                    $scope.party.location.latitude = marker.getPosition().lat();
+                    $scope.party.location.longitude = marker.getPosition().lng();
+                }
+            }
+        }
+    };
 });
--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -1,12 +1,15 @@
 /**
  * Created by waqas_000 on 12/6/2015.
  */
-angular.module('socialize').controller('PartiesListCtrl', ['$scope', '$meteor', function ($scope, $meteor, $rootScope, $state, $modal) {
+angular.module('socialize').controller('PartiesListCtrl', [
+    '$scope', '$meteor', '$rootScope', '$state', '$modal', function ($scope, $meteor, $rootScope, $state, $modal) {
 
     $scope.page = 1;
     $scope.perPage = 3;
     $scope.sort = {name: 1};
     $scope.orderProperty = '1';
+
+    $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
 
     $scope.parties = $meteor.collection(function () {
         return Parties.find({}, {
@@ -14,10 +17,56 @@ angular.module('socialize').controller('PartiesListCtrl', ['$scope', '$meteor', 
         });
     });
 
+    $meteor.autorun($scope, function () {
+        $meteor.subscribe('parties', {
+            limit: parseInt($scope.getReactively('perPage')),
+            skip: (parseInt($scope.getReactively('page')) - 1) * parseInt($scope.getReactively('perPage')),
+            sort: $scope.getReactively('sort')
+        }, $scope.getReactively('search')).then(function () {
+            $scope.partiesCount = $meteor.object(Counts, 'numberOfParties', false);
+        });
+    });
+
     $scope.$watch('orderProperty', function () {
         if ($scope.orderProperty)
             $scope.sort = {name: parseInt($scope.orderProperty)};
     });
+
+    $scope.rsvp = function (partyId, rsvp) {
+        $meteor.call('rsvp', partyId, rsvp).then(
+            function (data) {
+                console.log('success responding', data);
+            },
+            function (err) {
+                console.log('failed', err);
+            }
+        );
+    };
+
+    $scope.outstandingInvitations = function (party) {
+        return _.filter($scope.users, function (user) {
+            return (_.contains(party.invited, user._id) && !_.findWhere(party.rsvps, {user: user._id}));
+        });
+    };
+
+    $scope.getUserById = function (userId) {
+        return Meteor.users.findOne(userId);
+    };
+
+    $scope.creator = function (party) {
+        if (!party)
+            return;
+        var owner = $scope.getUserById(party.owner);
+        if (!owner)
+            return 'nobody';
+
+        if ($rootScope.currentUser)
+            if ($rootScope.currentUser._id)
+                if (owner._id === $rootScope.currentUser._id)
+                    return 'me';
+
+        return owner;
+    };
 
     $meteor.autorun($scope, function(){
         $meteor.subscribe("parties", {
@@ -43,7 +92,7 @@ angular.module('socialize').controller('PartiesListCtrl', ['$scope', '$meteor', 
     $scope.openAddNewPartyModal = function () {
         var modalInstance = $modal.open({
             animation: true,
-            templateUrl: 'client/parties/views/add-new-party-modal.html',
+            templateUrl: 'client/parties/views/add-new-party-model.html',
             controller: 'AddNewPartyCtrl',
             resolve: {
                 parties: function () {
